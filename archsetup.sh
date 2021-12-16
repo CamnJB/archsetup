cba85c78aeda932730c85ce9677bf655caca0cf3
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-16 21:29:43 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -3,23 +3,21 @@
 cd
 sudo mkdir ~/.config
 
-sudo  pacman -S firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
+sudo  pacman -S firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat /
 #download xmonad depend
-sudo pacman -S xorg-server xorg-apps xorg-xinit  xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf stack
+xorg-server xorg-apps xorg-xinit  xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf stack /
 #japanese fonts
-sudo pacman -S adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts  otf-ipafont ttf-hanazono ttf-sazanami
+adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts  otf-ipafont ttf-hanazono ttf-sazanami /
 #japanese inputs mozc
-sudo pacman -S fcitx-im fcitx-mozc
+fcitx-im fcitx-mozc
 
+#syLink dotfiles
 cd
 sudo git clone https://github.com/CamnJB/dotfiles
-sudo cp -L ~/dotfiles/.xinitrc ~/
-sudo cp -L ~/dotfiles/xmonad ~/.config/
-sudo cp -L ~/dotfiles/.xprofile ~/
-sudo cp -L ~/dotfiles/.vimrc ~/
-sudo cp -L ~/dotfiles/.bashrc ~/
+sudo cp -L ~/dotfiles/.xinitrc dotfiles/.xprofile ~/dotfiles/.vimrc ~/dotfiles/.bashrc ~/
 sudo cp -L ~/dotfiles/xmonad ~/.config/
 
+#clone & build xmonad
 cd ~/.config/xmonad
 sudo git clone https://github.com/xmonad/xmonad
 sudo git clone https://github.com/xmonad/xmonad-contrib
@@ -30,11 +28,12 @@ export PATH
 
 stack install
 
+#ani-cli clone & build
 cd ~/.config
 git clone https://github.com/pystardust/ani-cli.git
 cd ~/.config/ani-cli 
 sudo make
 
+#boot to xmonad
 cd
-
 startx
