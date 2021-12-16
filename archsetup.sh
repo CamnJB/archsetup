208c7f9fd32f8c3bb8385e82eae5a92b654edcd6
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-16 21:19:19 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 
 cd
-
-#make .config file
 sudo mkdir ~/.config
 
-#download packages
-sudo  pacman -S firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat stack
+sudo  pacman -S firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
 #download xmonad depend
-sudo pacman -S xorg-server xorg-apps xorg-xinit  xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf
+sudo pacman -S xorg-server xorg-apps xorg-xinit  xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf stack
 #japanese fonts
 sudo pacman -S adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts  otf-ipafont ttf-hanazono ttf-sazanami
 #japanese inputs mozc
 sudo pacman -S fcitx-im fcitx-mozc
 
-#download my dotfiles
-#my git repo
+cd
 sudo git clone https://github.com/CamnJB/dotfiles
+sudo cp -L ~/dotfiles/.xinitrc ~/
+sudo cp -L ~/dotfiles/xmonad ~/.config/
+sudo cp -L ~/dotfiles/.xprofile ~/
+sudo cp -L ~/dotfiles/.vimrc ~/
+sudo cp -L ~/dotfiles/.bashrc ~/
+sudo cp -L ~/dotfiles/xmonad ~/.config/
 
-#copy files to locations
-sudo cp ~/dotfiles/.xinitrc ~/ && sudo cp ~/dotfiles/xmonad ~/.config/ && sudo cp ~/dotfiles/.xprofile ~/ && sudo cp ~/dotfiles/.vimrc ~/ && sudo cp ~/dotfiles/.bashrc ~/ && sudo cp ~/dotfiles/xmonad ~/.config/
+cd ~/.config/xmonad
+sudo git clone https://github.com/xmonad/xmonad
+sudo git clone https://github.com/xmonad/xmonad-contrib
+sudo stack init
 
-#clone xmonad git
-sudo git clone https://github.com/xmonad/xmonad ~/.config/xmonad
-sudo git clone https://github.com/xmonad/xmonad-contrib ~/.config/monad
-
-cd ~/.config/xmonad && sudo stack init
-
-#set environment path
 PATH=$PATH:~/.local/bin
 export PATH
 
 stack install
 
-cd 
-
-#ani-cli
-git clone https://github.com/pystardust/ani-cli.git ~/.config
-
+cd ~/.config
+git clone https://github.com/pystardust/ani-cli.git
 cd ~/.config/ani-cli 
 sudo make
 
