d89e73e45aa1cb88e41af96b83ca3ba18ffed30f
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-17 02:32:00 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -4,9 +4,9 @@ cd
 sudo mkdir ~/.local
 sudo mkdir ~/.local/bin
 
-sudo pacman -S firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
+sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
 #download xmonad depend
-sudo pacman -S xorg-server xorg-apps xorg-xinit libx11 libxft libxinerama libxrandr libxss pkgconf stack
+sudo pacman -S xorg xorg-server xorg-apps xorg-xinit libx11 libxft libxinerama libxrandr libxss pkgconf stack
 #japanese fonts
 sudo pacman -S adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts  otf-ipafont ttf-hanazono ttf-sazanami
 #japanese inputs mozc
