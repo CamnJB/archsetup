7a377dd1fd6d66c1628e9fbf3d66aaefe24d8c9c
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-18 20:03:45 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -5,26 +5,12 @@ sudo mkdir ~/.local
 sudo mkdir ~/.local/bin
 
 sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
-#download xmonad depend
-sudo pacman -S xorg xorg-server xorg-apps xorg-xinit libx11 libxft libxinerama libxrandr libxss pkgconf stack
-#japanese fonts
-sudo pacman -S adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts  otf-ipafont ttf-hanazono ttf-sazanami
-#japanese inputs mozc
-sudo pacman -S fcitx-im fcitx-mozc
 
 #syLink dotfiles
 cd
 sudo git clone https://github.com/CamnJB/dotfiles.git
 sudo cp ~/dotfiles/dotfiles/.xinitrc dotfiles/dotfiles/.xprofile ~/dotfiles/dotfiles/.vimrc ~/dotfiles/dotfiles/.bashrc ~/
 sudo cp -r ~/dotfiles/dotfiles/.config ~/
-
-#clone & build xmonad
-cd ~/.config/xmonad
-sudo git clone https://github.com/xmonad/xmonad
-sudo git clone https://github.com/xmonad/xmonad-contrib
-sudo stack init
-export PATH=~/.local/bin:$PATH
-sudo stack install
 
 #ani-cli clone & build
 #cd ~/.config
