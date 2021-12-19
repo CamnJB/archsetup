7f939f157a948bd8f8b129974118b3a221f73a7d
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 00:17:54 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
+sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice 
+sudo pacman -S pipewire pipewire-alsa pipewire-pulse easyeffects 
+sudo pacman -S blender gimp newsboat
 
 ./1-xmonad.sh
 ./dotfiles.sh
