ea9e2387b33ebb9efc95307d68f93077c529721e
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 00:38:40 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -3,6 +3,8 @@
 sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice 
 sudo pacman -S blender gimp newsboat
 
+chmod +x ./1-xmonad.sh ./2-audio.sh ./dotfiles.sh ./ani-cli.sh ./JapaneseInput.sh
+
 ./1-xmonad.sh
 ./2-audio.sh
 ./dotfiles.sh
