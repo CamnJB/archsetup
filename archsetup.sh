5a2e1dca629e9ef3c5cd137d8e3bf182ec98da91
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 00:21:50 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice 
-sudo pacman -S pipewire pipewire-alsa pipewire-pulse easyeffects 
 sudo pacman -S blender gimp newsboat
 
 ./1-xmonad.sh
+./2-audio.sh
 ./dotfiles.sh
-./anicli.sh
+./ani-cli.sh
 ./JapaneseInput.sh
 
 #boot to xmonad
