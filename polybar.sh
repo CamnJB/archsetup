48415a8b917d29d19885aa5c93993078419c4c8c
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 02:25:43 +0000

    Update polybar.sh

--- polybar.sh
+++ polybar.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#####polybar stable#####
+sudo pacman -Syu
+git clone https://aur.archlinux.org/polybar.git  ~/.config/polybar
+cd ~/.config/polybar
+makepkg -sri
+#sudo pacman -U polybar.tar.xz
+cp /usr/share/doc/polybar/config ~/.config/polybar/
+
+
+#####polybar latest#####
+#sudo pacman -Syu
+#git clone https://aur.archlinux.org/polybar-git.git ~/.config/polybar
+#cd ~/.config/polybar
+#makepkg  -sri
+#sudo pacman -U polybar-git.tar.xz
+#cp /usr/share/doc/polybar/config ~/.config/polybar/
