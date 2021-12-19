946ea7d6da380f27aa36307a035461f37b232d2a
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 02:36:12 +0000

    Update 2-xmobar.sh

--- 2-xmobar.sh
+++ 2-xmobar.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+#####SOURCE#####
+mkdir -p ~/.config/xmobar
+git clone git://github.com/jaor/xmobar ~/.config/xmobar
+
+cd ~/.config/xmobar
+#cabal install -fall_extensions
+stack install --flag xmobar:all_extensions
