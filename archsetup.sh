be42d5dc0600b15c37ab52431bc05c8f254cdde1
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 00:15:34 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-cd
-sudo mkdir -p ~/.local/bin
-
 sudo pacman -S base-devel firefox rofi nitrogen picom alacritty mpv libreoffice pipewire pipewire-alsa pipewire-pulse easyeffects make blender gimp newsboat
 
-#syLink dotfiles
-cd
-sudo git clone https://github.com/CamnJB/dotfiles.git
-sudo cp ~/dotfiles/dotfiles/.xinitrc dotfiles/dotfiles/.xprofile ~/dotfiles/dotfiles/.vimrc ~/dotfiles/dotfiles/.bashrc ~/
-sudo cp -r ~/dotfiles/dotfiles/.config ~/
-
-#ani-cli clone & build
-#cd ~/.config
-#git clone https://github.com/pystardust/ani-cli.git
-#cd ~/.config/ani-cli 
-#sudo make
+./1-xmonad.sh
+./dotfiles.sh
+./anicli.sh
+./JapaneseInput.sh
 
 #boot to xmonad
 cd
