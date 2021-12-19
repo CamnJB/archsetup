ccc9bc0acdc9e9671fe8e0b294877661b23c2763
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-19 02:51:55 +0000

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,3 +3,5 @@
 git clone https://github.com/camnjb/dotfiles ~/
 cp ~/dotfiles/dotfiles/.bashrc ~/dotfiles/dotfiles/.vimrc ~//dotfilesdotfiles/.xinitrc ~//dotfilesdotfiles/.xprofile ~/
 cp ~/dotfiles/dotfiles/.config/xmonad/xmonad.hs ~/.config/xmonad && cp -r ~/dotfiles/dotfiles/.config/xmonad/lib ~/.config/xmonad/
+#xmobarrc
+cp ~/dotfiles/dotfiles/.config/xmobar/xmobarrc ~/.config/xmobar/
