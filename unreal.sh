0b3f270d870e3208ab654749058008b0b1c79180
CamnJB <82467513+CamnJB@users.noreply.github.com>
2022-04-05 03:27:43 +0100

    Update unreal.sh

--- unreal.sh
+++ unreal.sh
@@ -1,1 +1,10 @@
+#clone unreal github repo
+git clone https://github.com/EpicGames/UnrealEngine.git .config/UnrealEngine
+cd .config/UnrealEngine
 
+./Setup.sh
+./GenerateProjectFiles.sh
+make
+
+cd Engine/Binaries/Linux/
+UE4Editor
